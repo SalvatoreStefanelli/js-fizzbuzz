238fe65d679b2712fc32b1195b2e71372ac0682e
SalvatoreStefanelli <stefanellisalvatore.ss@gmail.com>
2023-12-01 21:31:42 +0100

    add Buzz to multiples of 5

--- script.js
+++ script.js
@@ -8,12 +8,15 @@ for (let i = 1; i <= 100; i++) {
     console.log(i);
 
 
- // Stampo Fizz per i multipli di 3   
+    // Stampo Fizz per i multipli di 3   
     if (i % 3 == 0) {
         console.log('Fizz');
+    
+    // Stampo Buzz per i multipli di 5
+    } else if (i % 5 == 0) {
+        console.log('Buzz');
     }
 
-// Stampo Buzz per i multipli di 5
 
 // Stampo FizzBuzz per i multipli di 3 e di 5
 }
