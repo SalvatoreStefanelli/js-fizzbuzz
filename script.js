c1bbd4e042106992505c5307b2bfb5f82c551d2a
SalvatoreStefanelli <stefanellisalvatore.ss@gmail.com>
2023-12-01 21:26:01 +0100

    add Fizz ai multipli di 3

--- script.js
+++ script.js
@@ -6,4 +6,14 @@
 // Ciclo For
 for (let i = 1; i <= 100; i++) {
     console.log(i);
+
+
+ // Stampo Fizz per i multipli di 3   
+    if (i % 3 == 0) {
+        console.log('Fizz');
+    }
+
+// Stampo Buzz per i multipli di 5
+
+// Stampo FizzBuzz per i multipli di 3 e di 5
 }
